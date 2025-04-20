f91059c1edba41cda8420d92a8c1f6e352a19378
Neeraj708888 <amck.sharma@gmail.com>
2025-04-20 23:28:00 +0530

    all done !

--- src/components/DeleteUser.jsx
+++ src/components/DeleteUser.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from 'react-router-dom';
 const DeleteUser = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const { users, loading, error } = useSelector((state) => state.user);
+  const { users, loading, error, search } = useSelector((state) => state.user);
 
   useEffect(() => {
     dispatch(getUserData());
@@ -17,6 +17,21 @@ const DeleteUser = () => {
       dispatch(deleteUserData(id));
     }
   };
+
+  // Search 
+  const filterUsers =
+    search.trim() === ''
+      ? users
+      : users.filter((user) =>
+      {
+        const searchTerm = search.toLowerCase();
+        return (
+          user.name?.toLowerCase().includes(searchTerm) ||
+          user.gender?.toLowerCase().includes(searchTerm) ||
+          String(user.age).includes(searchTerm)
+        );
+      }
+      );
 
   return (
     <div className="min-h-screen bg-gray-900 p-6">
@@ -37,7 +52,7 @@ const DeleteUser = () => {
         <p className="text-red-500 text-center">Error: {error}</p>
       ) : users && users.length > 0 ? (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {users.map((user) => (
+          {/* {users.map((user) => (
             <div
               key={user.id}
               className="bg-white rounded-xl shadow-lg p-5 space-y-2 text-gray-800 relative"
@@ -55,7 +70,31 @@ const DeleteUser = () => {
                 Delete
               </button>
             </div>
-          ))}
+          ))} */}
+
+            {
+              filterUsers.length > 0 ? (
+                filterUsers.map((user)=> (
+                  <div
+              key={user.id}
+              className="bg-white rounded-xl shadow-lg p-5 space-y-2 text-gray-800 relative"
+            >
+              <h3 className="text-xl font-semibold text-blue-700">{user.name}</h3>
+              <p><span className="font-medium">Email:</span> {user.email}</p>
+              <p><span className="font-medium">Age:</span> {user.age}</p>
+              <p><span className="font-medium">Mobile:</span> {user.mobile}</p>
+              <p><span className="font-medium">Gender:</span> {user.gender}</p>
+
+              <button
+                onClick={() => handleDelete(user.id)}
+                className="absolute top-2 right-2 bg-red-600 text-white px-3 py-1 rounded hover:bg-red-700 transition"
+              >
+                Delete
+              </button>
+            </div>
+                ))) : (<p>User not Found !</p>)
+            }
+
         </div>
       ) : (
         <p className="text-white text-center">No users available to delete.</p>
